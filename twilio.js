8a9b2fafd78a7bb71e5ef03818b8967ad7bc1974
unknown <xjkxkp1@gmail.com>
2025-07-10 13:49:43 +0700

    fix twilio-end

--- twilio.js
+++ twilio.js
@@ -1,12 +1,28 @@
-const twilio = require("twilio");
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const twilio = require('twilio');
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-async function callNumber(to) {
-  return client.calls.create({
-    url: process.env.TWIML_URL,
-    to,
+const callNumber = async (toNumber) => {
+  const twiml = `
+    <Response>
+      <Say voice="Polly.Kanya" language="th-TH">
+        สวัสดีค่ะ ขอแนะนำสมาชิกจากทาง PG DOG โปรโมชั่นพิเศษ วันเข้าพรรษา
+        โปรสายบุญ ฝากแล้วแตก (ไม่ต้องจุดธูป!)
+        ฝาก 88 รับ 188 ทำเทิร์น 4 เท่า ถอนได้สูงสุด 688 บาท อย่าลืมมารับโปรนะคะ
+      </Say>
+      <Hangup/>
+    </Response>
+  `;
+
+  const response = await client.calls.create({
+    to: toNumber,
     from: process.env.TWILIO_FROM_NUMBER,
+    twiml: twiml
   });
-}
+
+  return response;
+};
 
 module.exports = { callNumber };
